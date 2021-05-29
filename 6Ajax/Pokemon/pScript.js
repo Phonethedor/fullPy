bac5470ae37341b40dbdf25a72b6f284651dd3dc
Phonethedor <alvaroc.giovannim@gmail.com>
2021-05-29 12:35:25 -0400

    cual-pokemon

--- 6Ajax/Pokemon/pScript.js
+++ 6Ajax/Pokemon/pScript.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
     for (let x = 1; x<=151; x++){
-        $('#pokemon').append("<img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/"+x+".png'>")
+        $('#pokemon').append("<img id="+x+" src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/"+x+".png'>");
     }
+    $('img').click(function(){
+        $('#datos').empty();
+        let x = $(this).attr('id');
+        $.get('https://pokeapi.co/api/v2/pokemon/'+x+'/',function(res){
+            $('#datos').append('<h2>'+res.name+'</h2>');
+            $('#datos').append("<img id="+x+" src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/"+x+".png'><br>");
+            $('#datos').append('<h2>Tipos</h2>');
+            $('#datos').append('<ul>');
+            for (let i = 0; i<res.types.length;i++){
+                $('#datos').append('<li>'+res.types[i].type.name+'</li>');
+            }
+            $('#datos').append('</ul>')
+            $('#datos').append('<h2>Altura</h2>')
+            $('#datos').append('<p>'+res.height+'</p>');
+            $('#datos').append('<h2>Peso</h2>');
+            $('#datos').append('<p>'+res.weight+'</p>');
+        }, 'json');
+    });
 });
