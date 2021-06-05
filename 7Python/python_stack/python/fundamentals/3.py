a8737014a791dda1155a0432eac652e55d67bf47
Phonethedor <alvaroc.giovannim@gmail.com>
2021-06-04 21:18:29 -0400

    fin archivo 3

--- 7Python/python_stack/python/fundamentals/3.py
+++ 7Python/python_stack/python/fundamentals/3.py
@@ -21,3 +21,49 @@ if x <  10 :
 
 #para and, or y not, es la palabra literal
 
+#ciclo for
+#for x in range(0, 10, 1):
+#for x in range(0, 10):	 incremento de +1 está implícito
+#for x in range(10):	 incremento de +1 e inicia en 0 está inplicito
+for x in range(0, 10, 2):
+    print(x)
+# salida: 0, 2, 4, 6, 8
+for x in range(5, 1, -3):
+    print(x)
+# salida: 5, 2
+
+my_list = ["abc", 123, "xyz"]
+for i in range(0, len(my_list)):
+    print(i, my_list[i])
+# salida: 0 abc, 1 123, 2 xyz
+
+for v in my_list:
+    print(v)
+# salida: abc, 123, xyz
+
+#ciclo while
+count = 0
+while count < 5:
+    print("looping - ", count)
+    count += 1
+
+y = 3
+while y > 0:
+    print(y)
+    y = y - 1
+else:
+    print("Final de sentencia else")
+
+#break, al agregar break a un ciclo, se le indica que si se cumple el codigo, se debe salir del ciclo
+for val in "string":
+    if val == "i":
+        break
+    print(val)
+# salida: s, t, r
+
+#continue, si la condicion se cumple, vuelve al ciclo de inmediato
+for val in "string":
+    if val == "i":
+        continue
+    print(val)
+# salida: s, t, r, n, g
