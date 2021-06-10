3ed07b873287383605d26015283b878cf03a2257
Phonethedor <alvaroc.giovannim@gmail.com>
2021-06-09 21:58:10 -0400

    v1

--- 7Python/practica.py
+++ 7Python/practica.py
@@ -14,3 +14,59 @@ def calculadora():
     return print(msg)
 
 calculadora()
+
+#ingrese por pantalla el numero de productos que desea comprar luego ingrese el precio de cada producto
+#debe mostrar resultado de la compra del producto, agregando IVA al resultado final
+
+def market():
+    cantProducto = int(input('Ingrese la cantidad de productos que desea comprar : '))
+    precioProducto = int(input('Ingrese el precio del producto : '))
+    total = cantProducto+precioProducto
+    msg = 'Ud compro '+str(cantProducto)+' cada uno con el siguiente valor '+str(precioProducto)+' el total mas IVA es: '+str(total+total*0.19)
+    return print(msg)
+market()
+
+#utilice funciones para calcular lo siguiente:
+#ingrese por pantalla 4 notas, calcule el promedio de presentacion a examen
+#solicite la nota del examen, valide rango  de las notas entre 1 a 7
+#el alumno debe obtener en el examen al menos un 3.5 para aprobar el curso
+#el alumno aprueba con un promedio mayor o igual a 3.95
+# imprima el promedio y el estado del alumno aprobado o reprobado
+#calcule el promedio final de la nota de presentacion y la nota del examen
+#nota de presentacion es un 70% y el examen es 30%
+
+def notas():
+    n1 = float(input('Ingrese la primera nota: '))
+    if n1 <= 7 and n1 >= 1:
+        n2 = float(input('Ingrese la segunda nota: '))
+        if n2 <= 7 and n2 >= 1:
+            n3 = float(input('Ingrese la tercera nota: '))
+            if n3 <= 7 and n3 >= 1:
+                n4 = float(input('Ingrese la cuarta nota: '))
+                if n4 <= 7 and n4 >= 1:
+                    total = n1+n2+n3+n4
+                    prom = total/4
+                    print('su promedio de notas para presentacion a examen es: '+str(prom))
+                    if prom < 3.95:
+                        return print('ud ha reprobado el curso, sin posibilidad de rendir examen')
+                    else:
+                        examen = float(input('Ingrese su nota de examen: '))
+                        if examen <= 7 and examen >= 1:
+                            nf = prom*0.7 + examen*0.3
+                            print('Su nota final es: '+str(nf))
+                            if nf < 3.95:
+                                return print('ud ha reprobado el curso')
+                            else:
+                                return print('ud ha aprobado el curso, felicidades')
+                        else:
+                            return print('Nota inexistente')
+                else:
+                    return print('Nota inexistente')
+            else:
+                return print('Nota inexistente')
+        else:
+            return print('Nota inexistente')
+    else:
+        return print('Nota inexistente')
+
+notas()
---
+++ 7Python/python_stack/python/fundamentals/funciones_intermedias2.py
@@ -0,0 +1,56 @@
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+#Cambia el valor 10 en x a 15. Una vez que haya terminado, x ahora debería ser [[5,2,3], [15,8,9]].
+#Cambia el apellido del primer alumno de 'Jordan' a 'Bryant'
+#En el directorio sports_directory, cambia 'Messi' a 'Andres'
+#Camba el valor 20 en z a 30
+x [1][0] = 15
+students [0]['last_name'] = 'Bryant'
+sports_directory['soccer'][0] = 'Andres'
+z[0]['y'] = {'z' : 30}
+print(z)
+
+def iterateDictionary(somelist):
+    for valor in somelist:
+        print(valor)
+    
+
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+iterateDictionary(students) 
+# La salida debería ser: (Está bien si cada clave y valor quedan en dos líneas separadas)
+# Bonus: Hacer que aparezcan exactamente así!
+#first_name - Michael, last_name - Jordan
+#first_name - John, last_name - Rosales
+#first_name - Mark, last_name - Guillen
+#first_name - KB, last_name - Tonel
+
+def iterateDictionary (key,somelist):
+    for i in range(len(somelist)):
+        print(somelist[{key}])
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def printInfo (some_dict):
+    for i,j in some_dict.items():
+        list = j
+        print (len(list),i)
+        for x in list:
+            print(x)
+        print(x)
