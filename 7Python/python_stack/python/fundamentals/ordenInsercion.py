9cb2a0ce08c3ce445510f2c41c686cee7477674d
Phonethedor <alvaroc.giovannim@gmail.com>
2021-06-10 19:47:08 -0400

    ordenI

---
+++ 7Python/python_stack/python/fundamentals/ordenInsercion.py
@@ -0,0 +1,8 @@
+def insercion(list):
+    for i in range(1, len(list)):
+        x = list[i] #elemento a comparar
+        j = i - 1    
+        while j >= 0 and x < list[j]:
+            list[j + 1] = list[j]
+            j = j - 1
+        list[j + 1] = x
