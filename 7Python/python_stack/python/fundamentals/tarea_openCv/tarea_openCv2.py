7a6642ba648a82afa402955a2c0d15b9b99f5b4c
Phonethedor <alvaroc.giovannim@gmail.com>
2021-06-12 15:30:51 -0400

    final

--- 7Python/python_stack/python/fundamentals/tarea_openCv/tarea_openCv2.py
+++ 7Python/python_stack/python/fundamentals/tarea_openCv/tarea_openCv2.py
@@ -1,23 +1,23 @@
 import cv2
-# Load the cascade
+#añado el archivo cascade para caras frontales
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-# para capturar video desde la webCam
+#para capturar video desde la webCam
 cap = cv2.VideoCapture(0)
 while True:
-    # Analiza el cuadro
+    #Analiza el cuadro
     _, img = cap.read()
-    # lo convierto a escala de gris
+    #lo convierto a escala de gris
     gris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # detecta caras
+    #detecta caras
     caras = face_cascade.detectMultiScale(gris, 1.1, 4)
-    # añado rectangulo verde
+    #añado rectangulo verde
     for (x, y, w, h) in caras:
         cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    # muestro el resultado
+    #muestro el resultado
     cv2.imshow('img', img)
-    # se detiene si presiono escape
+    #se detiene si presiono escape
     k = cv2.waitKey(30) & 0xff
     if k==27:
         break        
-# fin de la captura
+#fin de la captura
 cap.release()
