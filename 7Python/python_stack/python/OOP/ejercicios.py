5ca2cc4261dbab99fce7411aaa3b3a94a82f3b8d
Phonethedor <alvaroc.giovannim@gmail.com>
2021-06-15 20:51:47 -0400

    v1

--- 7Python/python_stack/python/OOP/ejercicios.py
+++ 7Python/python_stack/python/OOP/ejercicios.py
@@ -1,19 +1,20 @@
-class Trabajador:
-    def __init__(self):
-        self.nombre = input("ingrese su nombre: ")
-        self.sueldo = int(input("Ingrese su sueldo: "))
-    def imprimir(self):
-        print("Nombre:",self.nombre)
-        print("Sueldo:",self.sueldo)
-    def bono(self):
-        if self.sueldo > 500000:
-            print('no aplica bono')
-        else:
-            bono = self.sueldo *0.15
-            self.sueldo += bono
-            print('su bono fue de:', bono)
-            print('su sueldo con bono es:', self.sueldo)
+#ctrl k+c para comentar el codigo marcado y ctrl k+u
+# class Trabajador:
+#     def __init__(self):
+#         self.nombre = input("ingrese su nombre: ")
+#         self.sueldo = int(input("Ingrese su sueldo: "))
+#     def imprimir(self):
+#         print("Nombre:",self.nombre)
+#         print("Sueldo:",self.sueldo)
+#     def bono(self):
+#         if self.sueldo > 500000:
+#             print('no aplica bono')
+#         else:
+#             bono = self.sueldo *0.15
+#             self.sueldo += bono
+#             print('su bono fue de:', bono)
+#             print('su sueldo con bono es:', self.sueldo)
 
-trabajador = Trabajador()
-trabajador.imprimir()
-trabajador.bono()
+# trabajador = Trabajador()
+# trabajador.imprimir()
+# trabajador.bono()
