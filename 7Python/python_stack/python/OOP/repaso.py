7303fdb0bfcc6e1bf07952bc1a0532b264563a72
Phonethedor <alvaroc.giovannim@gmail.com>
2021-07-02 19:26:04 -0400

    repaso

---
+++ 7Python/python_stack/python/OOP/repaso.py
@@ -0,0 +1,29 @@
+def agregarSueldos():
+    sueldos = []
+    for i in range(5):
+        moneda = int(input("Ingrese su sueldo : "))
+        sueldos.append(moneda)
+    return sueldos
+
+def impSueldos(sueldos):
+    for i in range (len(sueldos)):
+        print(sueldos[i])
+
+def contarSueldos(sueldos):
+    c = 0
+    for i in range (len(sueldos)):
+        if sueldos[i] > 1000:
+            c +=1
+    return c
+
+def promSueldos(sueldos):
+    sum = 0
+    for i in range (len(sueldos)):
+        sum += sueldos[i]
+    return sum/len(sueldos)
+
+def menorProm(sueldos):
+    prom = promSueldos(sueldos)
+    for i in range (len(sueldos)):
+        if sueldos[i] < prom:
+            print(sueldos[i])
