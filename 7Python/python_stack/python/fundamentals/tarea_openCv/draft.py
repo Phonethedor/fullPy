6f9d1ea4eeadc4763c43f5f5f2575c2f0dc5702a
Phonethedor <alvaroc.giovannim@gmail.com>
2021-07-22 20:49:06 -0400

    primer django

---
+++ 7Python/python_stack/python/fundamentals/tarea_openCv/draft.py
@@ -0,0 +1,25 @@
+import numpy as np
+import imageio
+import cv2
+import scipy.ndimage
+
+img='1.jpg'
+
+def grayscale(rgb):
+    return np.dot(rgb[...,:3],[0.299,0.587,0.114])
+
+def dodge(front,back):
+    result=front*255/(255-back)
+    result[result>255]=255
+    result[back==255]=255
+    return result.astype('uint8')
+
+s=cv2.imread(img)
+g=grayscale(s)
+i=255-g
+
+b = scipy.ndimage.filters.gaussian_filter(i,sigma=10)
+r = dodge(b,g)
+
+cv2.imshow(img, r)
+cv2.waitKey()
