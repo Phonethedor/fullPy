cf51926a8d0e6bbdb7d65ffb2cad210845d34e17
Phonethedor <alvaroc.giovannim@gmail.com>
2021-07-27 18:54:29 -0400

    tareaDate

---
+++ 8Django/proyectos_django/dateview/app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, HttpResponse
+from django.http import response
+from time import gmtime, strftime, localtime
+
+def date(request):
+    context = {
+        "time" : [
+            strftime("%m-%d-%y", localtime()),
+            strftime("%H: %M: %S", localtime())
+            ]
+    }
+
+    return render(request, 'dateview.html', context)
