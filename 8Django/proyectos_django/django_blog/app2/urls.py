33eeef08b6b981a66ccc9e3d519d8bdbfe35c18f
Phonethedor <alvaroc.giovannim@gmail.com>
2021-07-26 21:06:58 -0400

    cl

--- 8Django/proyectos_django/django_blog/app2/urls.py
+++ 8Django/proyectos_django/django_blog/app2/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from . import views
-from .views import inicio, info
+from .views import index,inicio, info
 
 urlpatterns = [
-    path('aplicacion/', views.index),
+    path('aplicacion/', index),
     path('inicio/', inicio.as_view, name='inicio'),
     path('info/', info.as_view, name='info'),
 ]
