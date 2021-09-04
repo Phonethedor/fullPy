6e39372bdbc98d9fc29fba3aae83b3bda2a0c434
Phonethedor <alvaroc.giovannim@gmail.com>
2021-09-04 09:36:26 -0400

    tareas

---
+++ 8Django/dojo_ninjas_proj/dojo_ninjas_app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class dojo(models.Model):
+    name = models.CharField(max_length = 255)
+    city = models.CharField(max_length = 255)
+    state = models.CharField(max_length = 2)
+    desc = models.TextField()
+
+
+class ninja(models.Model):
+    first_name = models.CharField(max_length = 255)
+    last_name = models.CharField(max_length = 255)
+    dojo = models.ForeignKey(dojo, related_name = "ninjas", on_delete =models.CASCADE)
